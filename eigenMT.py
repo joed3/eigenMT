983a0830626a8f8b7d015bf3b3bf433f02ae246e
Francois Aguet <francois@broadinstitute.org>
2019-02-26 14:22:26 -0500

    Changes for Python 3 compatibility

--- eigenMT.py
+++ eigenMT.py
@@ -1,10 +1,10 @@
+from __future__ import print_function
 import os
 import sys
 import fileinput
 import argparse
 import numpy as np
-import scipy as sp
-import scipy.linalg as splin 
+import scipy.linalg as splin
 import gc
 import gzip
 from sklearn import covariance
@@ -15,11 +15,11 @@ def open_file(filename):
 	'''
 	Function to open a (potentially gzipped) file.
 	'''
-	file_connection = open(filename)
+	file_connection = open(filename, 'rb')
 	file_header = file_connection.readline()
 	file_connection.close()
-	if file_header.startswith("\x1f\x8b\x08"):
-		file_connection = gzip.open(filename, 'rb')
+	if file_header.startswith(b"\x1f\x8b\x08"):
+		file_connection = gzip.open(filename, 'rt')
 	else:
 		file_connection = open(filename)
 	return file_connection
@@ -30,12 +30,12 @@ def make_genpos_dict(POS_fh, CHROM):
 	Keys are SNP IDs; values are positions.
 	Only stores the information of the SNPs from the given chromosome.
 	'''
- 
+
 	POS = open_file(POS_fh)
 	POS.readline()
 
 	pos_dict = {}
-	
+
 	for line in POS:
 		line = line.rstrip().split()
 		if line[1] == CHROM:
@@ -53,12 +53,12 @@ def make_phepos_dict(POS_fh, CHROM):
 	POS.readline()
 
 	pos_dict = {}
-	
+
 	for line in POS:
 		line = line.rstrip().split()
 		if line[1] == CHROM:
 			pos_array = np.array(line[2:4])
-			pos_dict[line[0]] = np.array(map(float, pos_array))
+			pos_dict[line[0]] = np.float64(pos_array)
 	POS.close()
 	return pos_dict
 
@@ -77,7 +77,7 @@ def make_gen_dict(GEN_fh, pos_dict):
 		snp = pos_dict[line[0]]
 		genos = np.array(line[1:])
 		genos[genos == 'NA'] = -1
-		genos = np.array(map(float, genos))
+		genos = np.float64(genos)
 		genos[genos == -1] = np.mean(genos[genos != -1])
 		gen_dict[snp] = genos
 	GEN.close()
@@ -95,7 +95,7 @@ def make_test_dict(QTL_fh, gen_dict, genpos_dict, phepos_dict, cis_dist):
 	header = QTL.readline().rstrip().split()
 	# find the column with the p-value
 	if 'p-value' in header:
-		pvalIndex = header.index('p-value') 
+		pvalIndex = header.index('p-value')
 	elif 'p.value' in header:
 		pvalIndex = header.index('p.value')
 	elif 'pvalue' in header:
@@ -132,14 +132,14 @@ def make_test_dict_external(QTL_fh, gen_dict, genpos_dict, phepos_dict, cis_dist
 	'''
 	Same arguments and output as the function above.
 	Main difference from the previous function is that it assumes the genotype matrix and position file
-	are separate from that used in the Matrix-eQTL run. This function is to be used with the external option 
+	are separate from that used in the Matrix-eQTL run. This function is to be used with the external option
 	to allow calculation of the effective number of tests using a different, preferably larger, genotype sample.
 	'''
 	QTL = open_file(QTL_fh)
 	header = QTL.readline().rstrip().split()
 	# find the column with the p-value
 	if 'p-value' in header:
-		pvalIndex = header.index('p-value') 
+		pvalIndex = header.index('p-value')
 	elif 'p.value' in header:
 		pvalIndex = header.index('p.value')
 	elif 'pvalue' in header:
@@ -167,7 +167,7 @@ def make_test_dict_external(QTL_fh, gen_dict, genpos_dict, phepos_dict, cis_dist
 							test_dict[gene]['best_snp'] = snp
 							test_dict[gene]['pval'] = pval
 							test_dict[gene]['line'] = '\t'.join(line)
-					
+
 	QTL.close()
 	snps = np.array(genpos_dict.values())
 	for gene in test_dict:
@@ -175,7 +175,7 @@ def make_test_dict_external(QTL_fh, gen_dict, genpos_dict, phepos_dict, cis_dist
 		#Calculate distances to phenotype start and end positions
 		is_in_cis_start = abs(snps - phepos[0]) <= cis_dist
 		is_in_cis_end = abs(snps - phepos[1]) <= cis_dist
-		test_dict[gene]['snps'] = snps[is_in_cis_start | is_in_cis_end] 
+		test_dict[gene]['snps'] = snps[is_in_cis_start | is_in_cis_end]
 	return test_dict, "\t".join(header)
 
 
@@ -198,13 +198,11 @@ def bf_eigen_windows(test_dict, gen_dict, phepos_dict, OUT_fh, input_header, var
 	TSSs = []
 	for gene in genes:
 		TSSs.append(phepos_dict[gene][0])
-	zipped = zip(TSSs, genes)
-	zipped.sort()
-	TSSs, genes = zip(*zipped)
+	TSSs, genes = [list(x) for x in zip(*sorted(zip(TSSs, genes), key=lambda p: p[0]))]
 	for gene in genes:
 		perc = (100 * counter / numgenes)
 		if (counter % 100) == 0:
-			print str(counter) + ' out of ' + str(numgenes) + ' completed ' + '(' + str(round(perc, 3)) + '%)' 
+			print(str(counter) + ' out of ' + str(numgenes) + ' completed ' + '(' + str(round(perc, 3)) + '%)')
 		sys.stdout.flush()
 		counter += 1
 		snps = np.sort(test_dict[gene]['snps'])
@@ -237,7 +235,7 @@ def bf_eigen_windows(test_dict, gen_dict, phepos_dict, OUT_fh, input_header, var
 		OUT.flush()
 		gc.collect()
 	OUT.close()
-	
+
 def lw_shrink(genotypes):
 	'''
 	Function to obtain smoothed estimate of the genotype correlation matrix.
@@ -247,7 +245,7 @@ def lw_shrink(genotypes):
 	'''
 	lw = covariance.LedoitWolf()
 	m, n = np.shape(genotypes)
-	try:	
+	try:
 		fitted = lw.fit(genotypes.T)
 		alpha = fitted.shrinkage_
 		shrunk_cov = fitted.covariance_
@@ -296,7 +294,7 @@ parser.add_argument('--external', dest = 'external', action = 'store_true', help
 
 args = parser.parse_args()
 
-QTL_fh = args.QTL 
+QTL_fh = args.QTL
 GEN_fh = args.GEN
 OUT_fh = args.OUT
 GENPOS_fh = args.GENPOS
@@ -308,34 +306,31 @@ cis_dist = float(args.cis_dist)
 external = args.external
 
 ##Make SNP position dict
-print 'Processing genotype position file.'
+print('Processing genotype position file.')
 sys.stdout.flush()
 genpos_dict = make_genpos_dict(GENPOS_fh, CHROM)
 
 ##Make phenotype position dict
-print 'Processing phenotype position file.'
+print('Processing phenotype position file.')
 sys.stdout.flush()
 phepos_dict = make_phepos_dict(PHEPOS_fh, CHROM)
 
 ##Make genotype dict
-print 'Processing genotype matrix.'
+print('Processing genotype matrix.')
 sys.stdout.flush()
 gen_dict = make_gen_dict(GEN_fh, genpos_dict)
 
 ##Make SNP-gene test dict
 if not external:
-	print 'Processing Matrix-eQTL tests file.'
+	print('Processing Matrix-eQTL tests file.')
 	sys.stdout.flush()
 	test_dict, input_header = make_test_dict(QTL_fh, gen_dict, genpos_dict, phepos_dict, cis_dist)
 else:
-	print 'Processing Matrix-eQTL tests file. External genotype matrix and position file assumed.'
+	print('Processing Matrix-eQTL tests file. External genotype matrix and position file assumed.')
 	sys.stdout.flush()
 	test_dict, input_header = make_test_dict_external(QTL_fh, gen_dict, genpos_dict, phepos_dict, cis_dist)
 
 ##Perform BF correction using eigenvalue decomposition of the correlation matrix
-print 'Performing eigenMT correction.'
+print('Performing eigenMT correction.')
 sys.stdout.flush()
 bf_eigen_windows(test_dict, gen_dict, phepos_dict, OUT_fh, input_header, var_thresh, window)
-
-
-
